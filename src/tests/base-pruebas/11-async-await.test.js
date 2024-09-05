77612d8b981c15c90b766c02167230a2ac0aee5d
SrMetus <s.gaggero87@gmail.com>
2024-09-05 01:46:23 -0400

    test 11-async-await

---
+++ src/tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,10 @@
+import { getImagen } from "../../base-pruebas/11-async-await"
+
+describe('Pruebas en 11-async-await', () => { 
+    test('getImagen debe retornar una url de la imagen', async() => {
+        const url = await getImagen();
+        console.log(url);
+
+        expect( typeof url ).toBe('string');
+    });
+ });
