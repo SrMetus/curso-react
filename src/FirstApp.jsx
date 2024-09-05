f07d4d64c34dcbb50f1fb6af6f9e943d71f3b55d
SrMetus <s.gaggero87@gmail.com>
2024-09-04 20:24:04 -0400

    props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,28 +1,29 @@
-const newMessage = [1,2,3,4,5,6,7,8,9]
-const newMessage2 = {
-    message: 'Hola mundos',
-    title: 'Sebastian',
-};
+// const newMessage = [1,2,3,4,5,6,7,8,9]
+// const newMessage2 = {
+//     message: 'Hola mundos',
+//     title: 'Sebastian',
+// };
 
-const date = new Date();
-const [month, day, year, hour, minute, second] = [
-    date.getMonth() + 1, 
-    date.getDate(),
-    date.getFullYear(),
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds()
-];
+// const date = new Date();
+// const [month, day, year, hour, minute, second] = [
+//     date.getMonth() + 1, 
+//     date.getDate(),
+//     date.getFullYear(),
+//     date.getHours(),
+//     date.getMinutes(),
+//     date.getSeconds()
+// ];
 
-export const FirstApp = () => {
+export const FirstApp = ({title, subtitle}) => {
 
     return (
         <>
-            <h1>First App</h1>
-            <h2>{`${day}-${month}-${year} at ${hour}:${minute}:${second}`}</h2>
+            <h1>{title}</h1>
+            {/* <h2>{`${day}-${month}-${year} at ${hour}:${minute}:${second}`}</h2>
             <h1>{newMessage}</h1>
-            <code>{JSON.stringify(newMessage2)}</code>
-            <p>Soy un subtitulo</p>
+            <code>{JSON.stringify(newMessage2)}</code> 
+            <p>Soy un subtitulo</p>*/}
+            <p>{subtitle * 2}</p>
         </>
     )
 };
