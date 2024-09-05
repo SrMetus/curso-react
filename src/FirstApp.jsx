fdb8335804a619b97b1f0e92726a093c00512caf
SrMetus <s.gaggero87@gmail.com>
2024-09-04 20:14:08 -0400

    insertar css

---
+++ src/FirstApp.jsx
@@ -0,0 +1,28 @@
+const newMessage = [1,2,3,4,5,6,7,8,9]
+const newMessage2 = {
+    message: 'Hola mundos',
+    title: 'Sebastian',
+};
+
+const date = new Date();
+const [month, day, year, hour, minute, second] = [
+    date.getMonth() + 1, 
+    date.getDate(),
+    date.getFullYear(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
+];
+
+export const FirstApp = () => {
+
+    return (
+        <>
+            <h1>First App</h1>
+            <h2>{`${day}-${month}-${year} at ${hour}:${minute}:${second}`}</h2>
+            <h1>{newMessage}</h1>
+            <code>{JSON.stringify(newMessage2)}</code>
+            <p>Soy un subtitulo</p>
+        </>
+    )
+};
