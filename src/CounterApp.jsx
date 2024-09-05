76211e12c686e291d8fa2125226b0e8715fc1687
SrMetus <s.gaggero87@gmail.com>
2024-09-04 21:15:28 -0400

    useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,13 +1,29 @@
+import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+
 export const CounterApp = ({value}) => {
+    
+    const [count, setCount] = useState(value);
+
+    const handleAdd = () => {
+        //console.log(event)
+        //setCount(count + 1);
+        setCount((prevCount) => prevCount + 1);
+    };
+    
+
     return (
-        <div>
+        <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
-        </div>
+            <h2> {count} </h2>
+            <button onClick={handleAdd}>
+                +1
+            </button>
+        </>
     );
-}
+};
 
 CounterApp.propTypes = {
     value: PropTypes.number.isRequired
