09c4e6da03dc4a1f3f0518e0fc7465122a280421
SrMetus <s.gaggero87@gmail.com>
2024-09-06 02:01:44 -0400

    simular eventos en CounterApp.js

--- src/tests/CounterApp.test.jsx
+++ src/tests/CounterApp.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../CounterApp';
 
 describe('Pruebas en CounterApp', () => { 
-    const initialValue = 100;
+    const initialValue = 10;
     test('debe hacer match con el snapshot', () => {
         const {container} = render(<CounterApp value={initialValue} />)
         expect(container).toMatchSnapshot();
@@ -10,6 +10,25 @@ describe('Pruebas en CounterApp', () => {
     test('debe mostrar el valor inicial de 100', () => {
         render(<CounterApp value={100} />);
         expect(screen.getByText(100)).toBeTruthy();
-        expect(screen.getByRole('heading', { level: 2}).innerHTML).toContain('100');
+        // expect(screen.getByRole('heading', { level: 2}).innerHTML).toContain('100');
+    });
+
+    test('debe incrementar con el boton +1', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('11')).toBeTruthy();
+    })
+
+    test('debe decrementar con el boton -1', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText('9')).toBeTruthy();
+    });
+
+    test('debe funcionar el bot de reset', () => {
+        render(<CounterApp value={69} />);
+        fireEvent.click(screen.getByRole('button', {name: 'btn-reset'}));
+        expect(screen.getByText(69)).toBeTruthy();
+
     });
  });
